79de3aaba2f8a0069508989763acecab40b364ea
Skye <poslert@oregonstate.edu>
2024-07-21 21:03:23 -0700

    Set up base front end nav

--- you-kan.UI/ClientApp/src/app/app.routes.ts
+++ you-kan.UI/ClientApp/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule, Component } from '@angular/core';
+import { ProjectListComponent } from './projects/components/project-list/project-list.component';
+import { UsersComponent } from './user-management/components/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'projects', component: ProjectListComponent },
+    { path: 'users', component: UsersComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)], 
+    exports: [RouterModule],
+    providers: []
+})
+
+export class Routing { }
