e2a9099e85bc58999059816d30aee66a09101eb2
Skye <poslert@oregonstate.edu>
2024-07-26 14:41:26 -0700

    Created base template for projects page.

---
+++ you-kan.UI/ClientApp/src/app/projects/components/project-list/project-list.ts
@@ -0,0 +1,10 @@
+import { Project } from "../../models/project.model";
+
+// change to endpoint to retrieve data
+
+export const project: Project[] = [
+    { id: 1, name: 'Project 1', description: 'test', startDate: 'Date', endDate: 'Date' },
+    { id: 2, name: 'Project 2', description: 'test', startDate: 'Date', endDate: 'Date' },
+    { id: 3, name: 'Project 3', description: 'test', startDate: 'Date', endDate: 'Date' }
+  ];
+
