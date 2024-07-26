87445d36993451ae35d60a34058b822287189cfc
cyreilv7 <shoiiru3@gmail.com>
2024-07-26 16:23:02 -0700

    Install jest and create integ tests for tasks crud

--- you-kan.Server/db/db-connector.js
+++ you-kan.Server/db/db-connector.js
@@ -8,6 +8,9 @@ const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 const { Sequelize } = require('sequelize');
 require('dotenv').config({ path: envFile });
 
+// bit of a hack since sequelize wants an object/function but .env file cand only contains strings
+const logger = process.env.LOGGER === 'false' ? false : console.log;
+
 const sequelize = new Sequelize(
   {
     database: process.env.DB_NAME,
@@ -17,6 +20,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: process.env.DIALECT,
     port: process.env.DB_PORT,
+    logging: logger,
   }
 );
 
--- you-kan.Server/server.js
+++ you-kan.Server/server.js
@@ -48,3 +48,5 @@ const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+module.exports = app;
