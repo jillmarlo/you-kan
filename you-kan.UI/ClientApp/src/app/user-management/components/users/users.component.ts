57f2b2071cd9a1b7ea97e42b39ce020d404eb5ce
Skye <poslert@oregonstate.edu>
2024-07-28 17:56:53 -0700

    Implemented users create functionality

--- you-kan.UI/ClientApp/src/app/user-management/components/users/users.component.ts
+++ you-kan.UI/ClientApp/src/app/user-management/components/users/users.component.ts
@@ -1,20 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import { User } from '../../models/user.model';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { UsersDetailComponent } from './users-detail.component';
 
 @Component({
   selector: 'app-users-component',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatTableModule, MatIconModule, MatButtonModule, MatDialogContent, MatDialogActions, MatDialogTitle, MatDialogClose
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
 
-  // replace witj endpoint
+  public dialog = inject(MatDialog);
+
+  // replace with endpoint
   userData: User[] = [
     {user_id: 1, first_name: 'John', last_name: 'Smith', email: 'jsmith@test.com', password_hash: 'testing123', created_at: 'Date here'}, 
     { user_id: 2, first_name: 'Jane', last_name: 'Doe', email: 'jdoe@test.com', password_hash: 'testing456', created_at: 'Date here'},
@@ -34,7 +39,12 @@ export class UsersComponent {
     // Implement delete logic
   }
 
-  addUser() {
-    
+  openUser(): void {
+    const sprintRef = this.dialog.open(UsersDetailComponent);
+
+    sprintRef.afterClosed().subscribe(result => {
+      if (result !== undefined) {
+      }
+    });
   }
 }
