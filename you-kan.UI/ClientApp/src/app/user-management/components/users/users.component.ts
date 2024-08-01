db5dbdefe5249bd04ec96c58d02d7fc4dc3fff5b
Skylar Posler <poslert@oregonstate.edu>
2024-08-01 16:36:59 -0700

    Adjusted display for users page

--- you-kan.UI/ClientApp/src/app/user-management/components/users/users.component.ts
+++ you-kan.UI/ClientApp/src/app/user-management/components/users/users.component.ts
@@ -20,6 +20,7 @@ export class UsersComponent {
   public dialog = inject(MatDialog);
 
   // replace with endpoint
+  // only display name &  email from sqlquery - test data has full info
   userData: User[] = [
     {user_id: 1, first_name: 'John', last_name: 'Smith', email: 'jsmith@test.com', password_hash: 'testing123', created_at: 'Date here'}, 
     { user_id: 2, first_name: 'Jane', last_name: 'Doe', email: 'jdoe@test.com', password_hash: 'testing456', created_at: 'Date here'},
