00cc0ce467ab0518cf2214c2cf21379c47ac1edf
Skylar Posler <poslert@oregonstate.edu>
2024-08-01 15:43:08 -0700

    Added WIP login page, adjusted padding in toolbar for logo.

--- you-kan.UI/ClientApp/src/app/app.routes.ts
+++ you-kan.UI/ClientApp/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ProjectListComponent } from './projects/components/project-list/project-list.component';
 import { UsersComponent } from './user-management/components/users/users.component';
 import { TaskBoardComponent } from './task-board/task-board.component';
+import { LoginComponent } from './user-management/components/login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'task-board', pathMatch: 'full' },
     { path: 'task-board', component: TaskBoardComponent },
     { path: 'users', component: UsersComponent },
     { path: 'projects', component: ProjectListComponent },
+    { path: 'login', component: LoginComponent},
 ];
