a8cc5fffeec344da7a6197629f9407146d142eab
Skye <poslert@oregonstate.edu>
2024-08-06 12:24:10 -0700

    Fixed scrollbar issue on task cards, added on brand favicon.

--- you-kan.UI/ClientApp/src/app/app.routes.ts
+++ you-kan.UI/ClientApp/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { UsersComponent } from './user-management/components/users/users.compone
 import { TaskBoardComponent } from './task-board/task-board.component';
 import { LoginComponent } from './user-management/components/login/login.component';
 import { SignUpComponent } from './user-management/components/sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent},
     { path: 'task-board', component: TaskBoardComponent },
     { path: 'users', component: UsersComponent },
     { path: 'projects', component: ProjectListComponent },
